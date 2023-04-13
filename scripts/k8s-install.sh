3180cdf83d2b38a69f72d62d5073de12220b8a3f
Rodrigo de Avila <rdeavila@gmail.com>
2023-04-13 16:06:53 -0300

    Add k8s install script

---
+++ scripts/k8s-install.sh
@@ -0,0 +1,79 @@
+#!/bin/env bash
+# curl https://raw.githubusercontent.com/rdeavila/rdeavila/main/scripts/k8s-install.sh | bash
+
+sudo systemctl stop firewalld
+sudo systemctl disable firewalld
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+cat <<EOF | sudo tee /etc/modules-load.d/k8s.conf
+br_netfilter
+overlay
+EOF
+
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo yum install vim wget nmap-ncat epel-release jq -y
+sudo yum update -y
+sudo wget https://github.com/mikefarah/yq/releases/latest/download/yq_linux_amd64 -O /usr/bin/yq && sudo chmod +x /usr/bin/yq
+
+cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-iptables  = 1
+net.bridge.bridge-nf-call-ip6tables = 1
+net.ipv4.ip_forward                 = 1
+EOF
+
+sudo sysctl --system
+
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io -y
+
+sudo mkdir -p /etc/docker
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+  "exec-opts": ["native.cgroupdriver=systemd"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m"
+  },
+  "storage-driver": "overlay2"
+}
+EOF
+
+containerd config default > /etc/containerd/config.toml
+
+# vim /etc/containerd/config.toml
+#[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
+#  ...
+#  [plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
+#    SystemdCgroup = true
+
+sudo systemctl daemon-reload
+sudo systemctl enable docker
+sudo systemctl restart docker
+sudo systemctl enable containerd
+sudo systemctl restart containerd
+
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://packages.cloud.google.com/yum/repos/kubernetes-el7-\$basearch
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+exclude=kubelet kubeadm kubectl
+EOF
+
+sudo yum install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+sudo systemctl enable --now kubelet
+
+echo ""
+echo "Run this command to start the controlplane"
+echo "   kubeadm init --control-plane-endpoint 192.168.1.149"
+echo ""
+
+exit 0
